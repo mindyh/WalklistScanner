2c20a175867289f1bed485eef2f9710d143c6b40
Jeff Barrera <jeff.barrera@gmail.com>
2018-10-19 23:44:35 -0700

    refactored scan.py main function and added correct output

--- scan.py
+++ scan.py
@@ -14,9 +14,11 @@ from enum import Enum
 import math
 import re
 import img2pdf
+import pprint
 
 import utils
 
+pp = pprint.PrettyPrinter(width=41)
 
 def parse_args():
   # construct the argument parser and parse the arguments
@@ -239,7 +241,7 @@ def get_circled_responses(response_bounding_box, response_codes, page, ref_respo
   return circled_responses, has_error
 
 
-def manual_review(response_bounding_box, page, circled_responses, voter_id):
+def manual_review(response_bounding_box, page, circled_responses, voter_id, response_codes):
   user_verdict = None
 
   top_margin = 50
@@ -258,18 +260,20 @@ def manual_review(response_bounding_box, page, circled_responses, voter_id):
       cv2.circle(responses_image, (resp.coords[0], resp.coords[1]+top_margin), 6, (0,0,255),-1)
 
     # convert to a string
-    response_pairs = ",".join(response_pairs)
+    response_string = ", ".join(response_pairs)
   else:
-    response_pairs = 'None'
+    response_string = 'None'
   
   # annotate the response image
   cv2.namedWindow("review", cv2.WINDOW_AUTOSIZE)
-  cv2.putText(responses_image, "Responses: {}. Is this correct? (y|n)".format(response_pairs), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+  responses_question = "Responses: {}. Is this correct? (y|n)".format(response_string)
+  cv2.putText(responses_image, responses_question, (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
   # keep looping until the y or n key is pressed
   while True:
     # display the image and wait for a keypress
     cv2.imshow("review", responses_image)
+    # key = input(responses_question + ": ").lower()
 
     key = cv2.waitKey(0) & 0xFF
    
@@ -283,12 +287,54 @@ def manual_review(response_bounding_box, page, circled_responses, voter_id):
     elif key == ord("n"):
       user_verdict = False
       print('{}: Incorrect scan :('.format(voter_id))
+      circled_responses = get_correct_responses(response_codes)
       break
 
   # close window  
   cv2.destroyAllWindows()
 
-  return user_verdict
+  print('correct_responses:')
+  print([code.value for code in circled_responses])
+
+  return user_verdict, circled_responses
+
+
+def get_correct_responses(response_codes):
+  print('=======================================================')
+  print("Please enter a comma-separated list of the correct responses. Enter 'n' if none.")
+
+  # build dict of question: values pairs (for printing nicely)
+  # and dict of answers_num: response pairs (for returning)
+  response_codes_by_question = {}
+  answers_to_code_objs = {}
+  for resp in response_codes:
+    # put in response_codes_by_question
+    if not resp.question_number in response_codes_by_question:
+      response_codes_by_question[resp.question_number] = []
+
+    unique_answer = "{}{}".format(resp.question_number, resp.value)
+
+    response_codes_by_question[resp.question_number].append(unique_answer)
+
+    # put in answers_to_code_objs
+    answers_to_code_objs[unique_answer.lower()] = resp
+
+  # print out to terminal
+  for question in response_codes_by_question:
+    print('QUESTION {}'.format(question))
+    print('   '.join(response_codes_by_question[question]))
+
+  # get user input
+  input_string = input('Enter correct responses: ').lower()
+  if input_string == 'n':
+    return []
+  else:
+    input_answers = input_string.split(',')
+    correct_responses = []
+    for answer in input_answers:
+      response_code = answers_to_code_objs[answer.strip()]
+      correct_responses.append(response_code)
+    return correct_responses
 
 
 # TODO: complete this function once have multi-response checking
@@ -384,6 +430,142 @@ def save_error_pages(error_pages, list_id):
     f.write(img2pdf.convert(error_images))
 
 
+def scan_page(args, page_number, ref_page, ref_bounding_boxes, list_dir, results_scans, results_stats, results_errors):
+  page = utils.load_page(utils.get_page_filename(args['list_id'], page_number), args["rotate_dir"])
+  response_codes = utils.load_response_codes(args['list_id'])
+
+  # align page
+  raw_page = page.copy()
+  page, transform = utils.alignImages(page, ref_page)
+  if utils.__DEBUG__:
+    utils.show_image(page)
+
+  # confirm page has the correct list_id
+  page_list_id = utils.get_list_id_from_page(page, ref_bounding_boxes["list_id"])
+  if page_list_id != args['list_id']:
+    print('Error: Page {} has ID {}, but active ID is {}. Page {} has been skipped.'.format(page_number, page_list_id, args['list_id'], page_number))
+    results_errors['skipped_pages'].append(raw_page)
+    return results_scans, results_stats, results_errors
+
+  # find the barcodes in the image and decode each of the barcodes
+  # Barcode scanner needs the unthresholded image.
+  barcodes = pyzbar.decode(page)
+  if len(barcodes) == 0:
+    print('Error: Cannot find barcodes. Page {} has been skipped.'.format(page_number))
+    results_errors['skipped_pages'].append(raw_page)
+    return results_scans, results_stats, results_errors
+
+  # loop over the detected barcodes
+  for barcode in barcodes:
+    results_scans, results_stats, results_errors = scan_barcode(barcode, page, ref_bounding_boxes, list_dir, response_codes, args, results_scans, results_stats, results_errors)
+    
+  if utils.__DEBUG__:
+    utils.show_image(page)
+
+  return results_scans, results_stats, results_errors
+
+
+def scan_barcode(barcode, page, ref_bounding_boxes, list_dir, response_codes, args, results_scans, results_stats, results_errors):
+  (barcode_coords, voter_id) = extract_barcode_info(barcode, page)
+
+  # increment barcodes counter
+  results_stats['num_scanned_barcodes'] += 1
+
+  if utils.__DEBUG__:
+    cv2.rectangle(page, barcode_coords[0], barcode_coords[1], (255, 0, 255), 3)
+    utils.show_image(page)
+
+  # Get the corresponding response codes region
+  response_bounding_box = get_response_for_barcode(barcode_coords, 
+                            ref_bounding_boxes["response_codes"])
+
+  # Figure out which ones are circled
+  ref_response_codes = utils.load_page(list_dir + utils.RESPONSE_CODES_IMAGE_FILENAME)
+  circled_responses, has_error = get_circled_responses(response_bounding_box, response_codes, page, ref_response_codes)
+  has_error = has_error or error_check_responses(circled_responses)
+
+  # if has an error at this point, add to the error tally
+  if has_error:
+    results_stats['num_error_barcodes'] += 1
+
+  # Do manual review if flagged and no errors
+  if args["manual_review"] and not has_error:
+    verdict_wrong, circled_responses = manual_review(response_bounding_box, page, circled_responses, voter_id, response_codes)
+
+    # if user verdict is false, add the voter_id to the list of incorrect scans
+    if verdict_wrong:
+      results_stats['incorrect_scans'].append(voter_id)
+
+  if has_error:
+    error_image = create_error_image(page, barcode_coords, ref_bounding_boxes["response_codes"])
+    results_errors['errors_for_human'].append(error_image)
+  else:
+    # save_responses(circled_responses, voter_id, dict_writer)
+    results_scans.append(build_results_dict(voter_id, circled_responses))
+
+  return results_scans, results_stats, results_errors
+
+
+def build_results_dict(voter_id, responses):
+  results = {}
+  results['voter_id'] = voter_id
+
+  question_to_responses = {}
+  for response in responses:
+    key = "question_{}".format(response.question_number)
+    if key not in question_to_responses:
+      question_to_responses[key] = []
+    question_to_responses[key].append(response.value)
+
+  results['questions'] = question_to_responses
+
+  return results
+
+
+def output_results_csv(list_id, list_dir, results_scans):
+
+  # get unique ordered list of questions
+  questions_and_answers = [scan['questions'] for scan in results_scans]
+  questions = list(set([k for d in questions_and_answers for k in d.keys()]))
+  questions.sort()
+
+  formatted_results = []
+  for scan in results_scans:
+    formatted_scan = {}
+    formatted_scan['primary_id'] = scan['voter_id']
+    for question in questions:
+      if question in scan['questions'].keys():
+        answer_set = scan['questions'][question]
+
+        for i, answer in enumerate(answer_set):
+          if i == 0:
+            colname = question
+          else:
+            colname = '{}_col{}'.format(question, i+1)
+          formatted_scan[colname] = answer
+      
+      formatted_results.append(formatted_scan)
+
+  # Prep the output file
+  all_colnames = sorted(set().union(*(d.keys() for d in formatted_results)))
+  with open("{}/results_{}.csv".format(list_dir, list_id), 'w+') as output_file:
+    dict_writer = csv.DictWriter(output_file, all_colnames)
+    dict_writer.writeheader()
+    dict_writer.writerows(formatted_results)
+
+
+def show_statistics(results_stats, args):
+  print('======== STATISTICS ========')
+  print('Scanned {} barcodes.'.format(results_stats['num_scanned_barcodes']))
+  print('{} ({}%) had system-detected errors.'.format(results_stats['num_error_barcodes'], round((results_stats['num_error_barcodes']/results_stats['num_scanned_barcodes'])*100)))
+
+  if args['manual_review']:
+    num_no_system_errors = results_stats['num_scanned_barcodes'] - results_stats['num_error_barcodes']
+    error_rate = len(results_stats['incorrect_scans']) / num_no_system_errors
+    accuracy_rate = round((1-error_rate)*100)
+    print('{} of {} were incorrectly scanned. {}% accuracy rate.'.format(len(results_stats['incorrect_scans']), num_no_system_errors, accuracy_rate))
+
+
 def main():
   args = parse_args()
   check_files_exist(args['list_id'])
@@ -392,95 +574,31 @@ def main():
   ref_bounding_boxes = utils.load_ref_boxes(args["list_id"])
   ref_page = utils.load_page(list_dir + utils.CLEAN_IMAGE_FILENAME)
 
-  # Prep the output file
-  outfile = open("{}/results_{}.csv".format(list_dir, args['list_id']), mode='w+')
-  dict_writer = csv.DictWriter(outfile, fieldnames=["voter_id", "question_1", "question_2", "question_3", "question_4"])
-  dict_writer.writeheader()
+  # init results object
+  results_scans = []
 
-  unreadable_responses_for_human = []
-  skipped_pages = []
-  incorrect_scans = []
-  num_scanned_barcodes = 0
-  num_error_barcodes = 0
+  # things to track for error reporting
+  results_stats = {}
+  results_stats['num_scanned_barcodes'] = 0
+  results_stats['num_error_barcodes'] = 0
+  results_stats['incorrect_scans'] = []
+
+  # stuff to build error PDF for human scanning
+  results_errors = {}
+  results_errors['errors_for_human'] = []
+  results_errors['skipped_pages'] = []
+
   num_pages = len(os.listdir("{}/{}".format(list_dir, utils.WALKLIST_DIR)))
   for page_number in range(args['start_page'], num_pages):
-    page = utils.load_page(utils.get_page_filename(args['list_id'], page_number), args["rotate_dir"])
-    response_codes = utils.load_response_codes(args['list_id'])
 
-    # align page
-    raw_page = page.copy()
-    page, transform = utils.alignImages(page, ref_page)
-    if utils.__DEBUG__:
-      utils.show_image(page)
+    results_scans, results_stats, results_errors = scan_page(args, page_number, ref_page, ref_bounding_boxes, list_dir, results_scans, results_stats, results_errors)
 
-    # confirm page has the correct list_id
-    page_list_id = utils.get_list_id_from_page(page, ref_bounding_boxes["list_id"])
-    if page_list_id != args['list_id']:
-      print('Error: Page {} has ID {}, but active ID is {}. Page {} has been skipped.'.format(page_number, page_list_id, args['list_id'], page_number))
-      skipped_pages.append(raw_page)
-      continue
+  output_results_csv(args['list_id'], list_dir, results_scans)
 
-    # find the barcodes in the image and decode each of the barcodes
-    # Barcode scanner needs the unthresholded image.
-    barcodes = pyzbar.decode(page)
-    if len(barcodes) == 0:
-      print('Error: Cannot find barcodes. Page {} has been skipped.'.format(page_number))
-      skipped_pages.append(raw_page)
-      continue
-
-    # loop over the detected barcodes
-    for barcode in barcodes:
-      (barcode_coords, voter_id) = extract_barcode_info(barcode, page)
-
-      # increment barcodes counter
-      num_scanned_barcodes += 1
-
-      if utils.__DEBUG__:
-        cv2.rectangle(page, barcode_coords[0], barcode_coords[1], (255, 0, 255), 3)
-        utils.show_image(page)
-
-      # Get the corresponding response codes region
-      response_bounding_box = get_response_for_barcode(barcode_coords, 
-                                ref_bounding_boxes["response_codes"])
-
-      # Figure out which ones are circled
-      ref_response_codes = utils.load_page(list_dir + utils.RESPONSE_CODES_IMAGE_FILENAME)
-      circled_responses, has_error = get_circled_responses(response_bounding_box, response_codes, page, ref_response_codes)
-      has_error = has_error or error_check_responses(circled_responses)
-
-      # if has an error at this point, add to the error tally
-      if has_error:
-        num_error_barcodes += 1
-
-      # Do manual review if flagged and no errors
-      if args["manual_review"] and not has_error:
-        has_error = manual_review(response_bounding_box, page, circled_responses, voter_id)
-
-        # if user verdict is false, add the voter_id to the list of incorrect scans
-        if has_error:
-          incorrect_scans.append(voter_id)
-
-      if has_error:
-        error_image = create_error_image(page, barcode_coords, ref_bounding_boxes["response_codes"])
-        unreadable_responses_for_human.append(error_image)
-      else:
-        save_responses(circled_responses, voter_id, dict_writer)
-
-    if utils.__DEBUG__:
-      utils.show_image(page)
-  outfile.close()
-
-  generate_error_pages(unreadable_responses_for_human, skipped_pages, args['list_id'])
+  generate_error_pages(results_errors['errors_for_human'], results_errors['skipped_pages'], args['list_id'])
 
   # print statistics
-  print('======== STATISTICS ========')
-  print('Scanned {} barcodes.'.format(num_scanned_barcodes))
-  print('{} ({}%) had system-detected errors.'.format(num_error_barcodes, round((num_error_barcodes/num_scanned_barcodes)*100)))
-  if args['manual_review']:
-    num_no_system_errors = num_scanned_barcodes - num_error_barcodes
-    error_rate = len(incorrect_scans) / num_no_system_errors
-    accuracy_rate = round((1-error_rate)*100)
-    print('{} of {} were incorrectly scanned. {}% accuracy rate.'.format(len(incorrect_scans), num_no_system_errors, accuracy_rate))
+  show_statistics(results_stats, args)
 
 if __name__ == '__main__':
   main()
